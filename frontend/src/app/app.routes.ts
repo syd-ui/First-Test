dcb4c0fb76dc681345f9364e57f44521ee37675b
Syd <gdjangangtchabet@gmail.com>
2026-05-09 09:06:30 +0100

    premier jenkins

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { Connexion } from './connexion/connexion';
+import { Component } from '@angular/core';
+import { Accueil } from './accueil/accueil';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'accueil',
+        component: Accueil
+    },
+
+    {
+        path:'**',
+        redirectTo:''
+    },
+
+    {
+        path:'',
+        component: App
+    }
+];
